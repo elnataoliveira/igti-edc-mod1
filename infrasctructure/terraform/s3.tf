853da016fae3c661699dd1c2993e3c6fa0918021
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-08-12 15:53:55 -0300

    Update s3.tf

--- infrasctructure/terraform/s3.tf
+++ infrasctructure/terraform/s3.tf
@@ -1,1 +1,19 @@
+resource "aws_s3_bucket" "dl" {
+  bucket = "<datalake-name>",
+  acl    =  "private"
+  
+  tags = {
+    <TAG> = "NAME",
+    <TAG> = "NAME"
+  }
+  
+  server_side_encryption_configuration_by_default{
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
 
